1e20e741b9129b9b770d091ed9db1539c0cf6837
Boris Kirov <borisasenovkirov@gmail.com>
2025-01-27 16:54:18 +0100

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
+import styles from "./page.module.css";
+import { useRef } from "react";
 
-export default function Home() {
+export default function Index() {
+  let steps = 0;
+  let currentIndex = 0;
+  let nbOfImages = 0;
+  let maxNumberOfImages = 10;
+  let refs = [];
+
+  const manageMouseMove = (e) => {
+    const { clientX, clientY, movementX, movementY } = e;
+
+    steps += Math.abs(movementX) + Math.abs(movementY);
+
+    if (steps >= currentIndex * 150) {
+      moveImage(clientX, clientY);
+
+      if (nbOfImages == maxNumberOfImages) {
+        removeImage();
+      }
+    }
+
+    if (currentIndex == refs.length) {
+      currentIndex = 0;
+      steps = -150;
+    }
+  };
+
+  const moveImage = (x, y) => {
+    const currentImage = refs[currentIndex].current;
+    currentImage.style.left = x + "px";
+    currentImage.style.top = y + "px";
+    currentImage.style.display = "block";
+    currentIndex++;
+    nbOfImages++;
+    setZIndex();
+  };
+
+  const setZIndex = () => {
+    const images = getCurrentImages();
+    for (let i = 0; i < images.length; i++) {
+      images[i].style.zIndex = i;
+    }
+  };
+
+  const removeImage = () => {
+    const images = getCurrentImages();
+    images[0].style.display = "none";
+    nbOfImages--;
+  };
+
+  const getCurrentImages = () => {
+    let images = [];
+    let indexOfFirst = currentIndex - nbOfImages;
+    for (let i = indexOfFirst; i < currentIndex; i++) {
+      let targetIndex = i;
+      if (targetIndex < 0) targetIndex += refs.length;
+      images.push(refs[targetIndex].current);
+    }
+    return images;
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <h1>SUMMER IN THE CITY</h1>
+      <div
+        onMouseMove={(e) => {
+          manageMouseMove(e);
+        }}
+        className={styles.main}
+      >
+        {[...Array(20).keys()].map((_, index) => {
+          const ref = useRef(null);
+          refs.push(ref);
+          return (
+            <img
+              onClick={() => {
+                console.log(refs);
+              }}
+              ref={ref}
+              src={`/images/${index}.jpg`}
+            ></img>
+          );
+        })}
+      </div>
+    </main>
   );
 }
